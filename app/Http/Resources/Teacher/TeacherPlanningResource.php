02ccb5204506085c02b8dab87c324a29d52f042a
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2024-12-27 19:18:40 -0500

    cambios readme

--- app/Http/Resources/Teacher/TeacherPlanningResource.php
+++ app/Http/Resources/Teacher/TeacherPlanningResource.php
@@ -21,28 +21,30 @@ class TeacherPlanningResource extends JsonResource
             'job_position_id' => $this->job_position_id,
             'complementaries' => $this->complementaries->map(function ($value) {
                 $dataSub = [];
-                $subject_ids = explode(',', $value->subject_ids);
+                $subject_ids = explode(',', $value->subject_ids); 
                 foreach ($subject_ids as $key => $sub) {
-                    $x = Subject::find($sub);
-
-                    $files = TeacherPlanning::where(function ($query) use ($value, $x) {
-                        $query->where('teacher_id', $value->teacher_id);
-                        $query->where('grade_id', $value->grade_id);
-                        $query->where('section_id', $value->section_id);
-                        $query->where('subject_id', $x->id);
-                    })->get()->map(function ($f) {
-                        return [
-                            'id' => $f->id,
-                            'name' => $f->name,
-                            'file' => $f->path,
+                    $x = Subject::find($sub); 
+                    if($x){
+                        $files = TeacherPlanning::where(function ($query) use ($value, $x) {
+                            $query->where('teacher_id', $value->teacher_id);
+                            $query->where('grade_id', $value->grade_id);
+                            $query->where('section_id', $value->section_id);
+                            $query->where('subject_id', $x->id);
+                        })->get()->map(function ($f) {
+                            return [
+                                'id' => $f->id,
+                                'name' => $f->name,
+                                'file' => $f->path,
+                            ];
+                        });
+    
+                        $dataSub[] = [
+                            'value' => $sub,
+                            'title' => $x->name,
+                            'files' => $files,
                         ];
-                    });
+                    }
 
-                    $dataSub[] = [
-                        'value' => $sub,
-                        'title' => $x->name,
-                        'files' => $files,
-                    ];
                 }
 
                 return [
