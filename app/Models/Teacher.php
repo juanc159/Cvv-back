e8c965a685c5bfd2873bb28012f2809febad40a6
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-02-20 18:30:50 -0500

    cambios funcionales

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use App\Traits\Searchable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory; 
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Teacher extends Model
+
+class Teacher extends Authenticatable
 {
     use HasApiTokens, HasFactory, HasUuids, Searchable, SoftDeletes;
 
