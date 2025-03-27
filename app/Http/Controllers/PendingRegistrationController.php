efdfcf4d5d5caa9467de1dc723461b4927fdd4d6
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-03-26 20:35:27 -0500

    cambios funcionales

--- app/Http/Controllers/PendingRegistrationController.php
+++ app/Http/Controllers/PendingRegistrationController.php
@@ -433,7 +433,7 @@ class PendingRegistrationController extends Controller
             ];
         }
 
-        return $attempts;
+        // return $attempts;
 
         $excel = Excel::raw(new PendingRegistrationExport($responseData, $attempts), \Maatwebsite\Excel\Excel::XLSX);
 
