29ede3f59389b2a5d14d6fc8be04b2dcc24a8252
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-03-26 20:33:02 -0500

    cambios funcionales

--- app/Http/Controllers/PendingRegistrationController.php
+++ app/Http/Controllers/PendingRegistrationController.php
@@ -377,27 +377,11 @@ class PendingRegistrationController extends Controller
 
         foreach ($data as $key => $pendingRegistration) {
 
-            return$pendingRegistration = $this->pendingRegistrationRepository->find($pendingRegistration->id);
+             $pendingRegistration = $this->pendingRegistrationRepository->find($pendingRegistration->id);
 
-            $attempts = $this->pendingRegistrationAttemptRepository->list([
+           return $attempts = $this->pendingRegistrationAttemptRepository->list([
                 "pending_registration_id" => $pendingRegistration->id
-            ], ['student', 'subject'])->map(function ($attempt) {
-                return [
-                    'id' => $attempt->id,
-                    'student_id' => [
-                        'value' => $attempt->student->id,
-                        'title' => $attempt->student->full_name,
-                    ],
-                    'subject_id' => [
-                        'value' => $attempt->subject->id,
-                        'title' => $attempt->subject->name,
-                    ],
-                    'attempt_number' => $attempt->attempt_number, // Asegúrate de incluir este campo
-                    'note' => $attempt->note,
-                    'attempt_date' => Carbon::parse($attempt->attempt_date)->format("d-m-Y"),
-                    'approved' => $attempt->approved,
-                ];
-            });
+            ]);
 
 
             // Fetch associated students and their subjects 
