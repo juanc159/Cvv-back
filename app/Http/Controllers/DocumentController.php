ae746d63112cb7a3644dca933fafebd51e927ecb
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-05-19 15:47:37 -0500

    cambios

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -194,7 +194,7 @@ class DocumentController extends Controller
             $student->full_name = str_replace(',', '', $student->full_name);
 
             $type_document = $student->type_document?->name;
-            $type_document_name = " del documento ";
+            $type_document_name = "";
             switch ($type_document) {
                 case 'Cédula de identidad':
                     $type_document_name = " de la cédula de identidad";
@@ -207,7 +207,7 @@ class DocumentController extends Controller
                     break;
 
                 default:
-                    $type_document_name = "";
+                    $type_document_name = " del documento ";
                     break;
             }
             $student->type_document_name = $type_document_name;
